cce4aa8958beb03ca8d9995c6d359bfb6208d1bb
Yokool <letsgamecze@gmail.com>
2023-11-13 12:32:23 +0100

    commit last file

--- src/TaskDateList/CompleteTaskRow.tsx
+++ src/TaskDateList/CompleteTaskRow.tsx
@@ -2,27 +2,32 @@ import React, { useContext } from "react";
 import { DateTableSelectionRow } from "./DateTableSelectionRow";
 import { Task } from "../TaskTypes/Task";
 import './TaskDateList.css';
-import { TaskFulfillment, TaskFulfillmentContext, addRowParameterToEachFulfillment, getAssociatedFulfillmentsToTask } from "../TaskFulfillment/TaskFulfillment";
+import { TaskFulfillment, TaskFulfillmentContext, addRowParameterToEachFulfillment, clampTaskfulfillmentsToDates, getAssociatedFulfillmentsToTask } from "../TaskFulfillment/TaskFulfillment";
 import { CellSpacerTD, DefaultCellHeight, TaskTableCodeCellTD, TaskTableNameCellTD } from "./TaskDateListStyledComponents";
 
 export type CompleteTaskRowProps = {
     task: Task
     surroundingDates: Date[]
     isLastRow: boolean
+    dateListStart: Date,
+    dateListEnd: Date
 }
 
 export type CommonTaskRowCellStyle = {
     height: number
 }
 
-export function CompleteTaskRow({task, surroundingDates, isLastRow}: CompleteTaskRowProps): JSX.Element
+export function CompleteTaskRow({
+    task, surroundingDates, isLastRow, dateListStart, dateListEnd
+}: CompleteTaskRowProps): JSX.Element
 {
 
     const taskFulfillmentList = useContext(TaskFulfillmentContext);
     const fulfillmentsInThisRow = getAssociatedFulfillmentsToTask(task, taskFulfillmentList);
 
     const {taskfulfillmentsParameterized, subrowCount} = addRowParameterToEachFulfillment(fulfillmentsInThisRow);
-    
+    const taskfulfillmentsParameterizedClamped = clampTaskfulfillmentsToDates(taskfulfillmentsParameterized, dateListStart, dateListEnd);
+
     // Either use the default when no tasks have been added
     // or make enough space for all the tasks
     const commonHeight = subrowCount === 0 ? DefaultCellHeight : (subrowCount * DefaultCellHeight)
@@ -44,7 +49,7 @@ export function CompleteTaskRow({task, surroundingDates, isLastRow}: CompleteTas
             completeDateList={surroundingDates}
             task={task}
             commonCellStyle={commonHeightStyle}
-            parameterizedTaskFulfillmentList={taskfulfillmentsParameterized}
+            parameterizedTaskFulfillmentList={taskfulfillmentsParameterizedClamped}
             subRowCount={subrowCount}
             isLastRow={isLastRow}
         />
